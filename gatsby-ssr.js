e12c3f0af9167f3dfc50f8bffa2f33f7f64304b5
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-08-26 14:26:48 +0000

    final try of today index.html

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,7 @@
 export { default as wrapPageElement } from './wrapPageElement';
 export { default as wrapRootElement } from './wrapRootElement';
 
+
 import React from 'react'
 
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
@@ -12,6 +13,7 @@ export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) =>
       dangerouslySetInnerHTML={{
         __html: `
         <!-- Meta Pixel Code -->
+        <script>
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -22,26 +24,18 @@ export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) =>
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '517310537646754');
         fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=517310537646754&ev=PageView&noscript=1"
+        /></noscript>
         <!-- End Meta Pixel Code -->
         `,
       }}
     />
   )
 
-  const noscriptPixel = (
-    <noscript key="noscript-pixel">
-      <img 
-        height="1" 
-        width="1" 
-        style={{display: 'none'}}
-        src="https://www.facebook.com/tr?id=517310537646754&ev=PageView&noscript=1"
-      />
-    </noscript>
-  )
-
-  replaceHeadComponents([...headComponents, pixelScript, noscriptPixel])
+  replaceHeadComponents([...headComponents, pixelScript])
 }
-
 
 
 
@@ -56,23 +50,23 @@ export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) =>
 //       key="pixel-meta"
 //       dangerouslySetInnerHTML={{
 //         __html: `
-//         <!-- Meta Pixel Code -->
-//         <script>
-//         !function(f,b,e,v,n,t,s)
-//         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-//         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-//         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-//         n.queue=[];t=b.createElement(e);t.async=!0;
-//         t.src=v;s=b.getElementsByTagName(e)[0];
-//         s.parentNode.insertBefore(t,s)}(window, document,'script',
-//         'https://connect.facebook.net/en_US/fbevents.js');
-//         fbq('init', '517310537646754');
-//         fbq('track', 'PageView');
-//         </script>
-//         <noscript><img height="1" width="1" style="display:none"
-//         src="https://www.facebook.com/tr?id=517310537646754&ev=PageView&noscript=1"
-//         /></noscript>
-//         <!-- End Meta Pixel Code -->
+        // <!-- Meta Pixel Code -->
+        // <script>
+        // !function(f,b,e,v,n,t,s)
+        // {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        // n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        // if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        // n.queue=[];t=b.createElement(e);t.async=!0;
+        // t.src=v;s=b.getElementsByTagName(e)[0];
+        // s.parentNode.insertBefore(t,s)}(window, document,'script',
+        // 'https://connect.facebook.net/en_US/fbevents.js');
+        // fbq('init', '517310537646754');
+        // fbq('track', 'PageView');
+        // </script>
+        // <noscript><img height="1" width="1" style="display:none"
+        // src="https://www.facebook.com/tr?id=517310537646754&ev=PageView&noscript=1"
+        // /></noscript>
+        // <!-- End Meta Pixel Code -->
 //         `,
 //       }}
 //     />,
