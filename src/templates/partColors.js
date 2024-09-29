b9117c82064c6667ad3ad376f5c4c89e659fca34
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-09-29 13:40:47 +0000

    change part intro background

--- src/templates/partColors.js
+++ src/templates/partColors.js
@@ -1,14 +1,14 @@
 export const partColors = {
-  0: 'light-violet',
+  0: 'dark-orange',
   1: 'green',
-  2: 'dark-orange',
-  3: 'light-orange',
+  2: 'light-violet',
+  3: 'light-green',
   4: 'yellow',
   5: 'pink',
   6: 'violet',
   7: 'light-blue',
   8: 'turquoise',
-  9: 'light-green',
+  9: 'light-orange',
   10: 'part10-light-blue',
   11: 'purple'
 };
