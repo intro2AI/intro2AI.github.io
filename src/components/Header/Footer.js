bafd84fa94435ac387e079ec3e31d5ed26ef380a
Intro 2 AI <58814576+intro2AI@users.noreply.github.com>
2024-09-30 13:19:06 +0000

    button black always

--- src/components/Header/Footer.js
+++ src/components/Header/Footer.js
@@ -1,88 +1,34 @@
 import './Footer.scss';
-
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
-import FooterNavigation from '../Navigation/FooterNavigation';
 import { TripleBorder } from '../TripleBorder/TripleBorder';
 import getTranslationPath from '../../utils/getTranslationPath';
-import PaymentButton from '../IndexPage/PaymentButton';
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
+const Footer = () => {
+  const lang = 'fi';
 
-    this.state = {
-      top: 0,
-      footerClass: '',
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  footer = React.createRef();
-
-  handleScroll = () => {
-    let scroll = window.scrollY;
-
-    this.setState({
-      top: scroll,
-      footerClass:
-        this.state.top > 5 && scroll > this.state.top ? 'footer--small' : '',
-    });
-  };
-
-  render() {
-    const { footerClass } = this.state;
-    // const { lang } = this.props;
-    const lang = 'fi';
-
-    const isSmallFooter = footerClass === 'footer--small';
-
-    return (
+  return (
+    <div className="footer footer--small" style={{ backgroundColor: 'transparent' }}>
       <div
-        ref={this.footer}
-        className={`footer ${footerClass}`}
+        className="container"
         style={{
-          backgroundColor: footerClass !== '' ? 'transparent' : 'transparent',
+          alignItems: 'center',
+          justifyContent: 'center',
+          display: 'flex',
         }}
       >
-        <div
-          className="container"
-          style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            display: 'flex',
-          }}
-        >
-          <Link to={getTranslationPath(lang, '/about')} className="footer__logo">
-            {footerClass === '' ? (
-              <TripleBorder
-                className="nav-item-hover"
-                childrenClassName="triple-border__logo"
-              >
-                {'Buy'}
-              </TripleBorder>
-            ) : (
-              <TripleBorder
-                hover
-                className="nav-item-hover"
-                childrenClassName="triple-border__logo"
-              >
-                Buy
-              </TripleBorder>
-            )}
-          </Link>
-          {/* {!isSmallFooter && <FooterNavigation lang={lang} />} */}
-        </div>
+        <Link to={getTranslationPath(lang, '/about')} className="footer__logo">
+          <TripleBorder
+            hover
+            className="nav-item-hover"
+            childrenClassName="triple-border__logo"
+          >
+            Buy
+          </TripleBorder>
+        </Link>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Footer;
